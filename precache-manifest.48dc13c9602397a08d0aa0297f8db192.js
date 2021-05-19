30ba8acc7b35e497f580188cc0ef006e328347aa
fasttxt <84473551+fasttxt@users.noreply.github.com>
2021-05-20 00:57:50 +0530

    Added 1st file

---
+++ precache-manifest.48dc13c9602397a08d0aa0297f8db192.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f967735455e7642d8782b46064f0f7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1809c4fe457170a27cf",
+    "url": "/static/css/main.e3f8f100.chunk.css"
+  },
+  {
+    "revision": "3cdcbca6f670467176a0",
+    "url": "/static/js/2.0fc92f25.chunk.js"
+  },
+  {
+    "revision": "d1809c4fe457170a27cf",
+    "url": "/static/js/main.e9c00fca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a2f52ab65d52a60e3c62ae04ea02cf07",
+    "url": "/static/media/chat.a2f52ab6.svg"
+  },
+  {
+    "revision": "8b02c0d90c773ebd6fecae8e886fb2d0",
+    "url": "/static/media/loginCrt.8b02c0d9.png"
+  }
+]);
